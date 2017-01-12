917c58f57ff6e2383c7ed5d29961988c8048d2a1
cameronmhenkel <cameron.m.henkel@gmail.com>
2017-01-12 14:24:03 -0600

    Team 007 Algorithm
    Collude then copy opponents last move.

--- team7.py
+++ team7.py
@@ -25,8 +25,12 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if len(my_history)==0:
+        return 'c'
+    elif their_history[-1]=='b':
+        return 'b'
+    elif their_history[-1]=='c':
+        return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
